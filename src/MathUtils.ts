c6e5a10032ac0ab071fff7691ca248a87075c2c3
Dani Nebot <daniign@gmail.com>
2016-08-31 22:51:28 +0200

    Add MathUtils mini lib

---
+++ src/MathUtils.ts
@@ -0,0 +1,16 @@
+export module MathUtils {
+
+  // https://gist.github.com/caseyjustus/1166258
+  export function median(values: Array<number>) {
+    values.sort(function(a, b) { return a - b; });
+
+    var half = Math.floor(values.length / 2);
+
+    if (values.length % 2) {
+      return values[half];
+    }
+    else {
+      return (values[half - 1] + values[half]) / 2.0;
+    }
+  }
+}
